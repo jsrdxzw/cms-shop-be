deebd17afc9bc83526159c7928a36842c9bc0efb
jsrdxzw <jsrdxzw@163.com>
2021-01-25 22:45:01 +0800

    init project

---
+++ src/main/java/com/jsrdxzw/cmsshopbe/CmsShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.jsrdxzw.cmsshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsShopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsShopBeApplication.class, args);
+    }
+
+}
